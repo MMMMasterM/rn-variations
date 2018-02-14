c416d14db43124eb747b870569214b405127a932
MMM <ab@c.de>
2018-02-14 22:35:46 +0100

    implemented training and validation

--- train.py
+++ train.py
@@ -2,6 +2,18 @@ import tensorflow as tf
 import numpy as np
 import os, pickle
 
+#load dictionary
+with open(os.path.join('processeddata', 'dictionary.txt'), 'rb') as f:
+    wordIndices = pickle.load(f)
+dictSize = len(wordIndices) + 1#padding entry with index 0 is not listed in wordIndices
+
+#load data
+with open(os.path.join('processeddata', 'train.txt'), 'rb') as f:
+    trainingData = pickle.load(f)
+
+with open(os.path.join('processeddata', 'valid.txt'), 'rb') as f:
+    validationData = pickle.load(f)
+
 #training parameters
 batch_size = 32
 epoch_count = 10
@@ -434,6 +446,27 @@ def buildWordProcessorLSTMs():
     #return inputContext, inputContextLengths, inputQuestion, inputQuestionLengths, answer, answerGates
     return inputContext, inputContextLengths, inputContextSentenceLengths, inputQuestion, inputQuestionLengths, sentenceLSTMoutputs, questionLSTMoutputs
 
+def buildAnswerModel(prevNetworkOutput):
+    #tf.nn.softmax removed because it's applied afterwards by the built-in loss function softmax_cross_entropy_with_logits
+    #TODO: make a second output WITH softmax for validation/testing - not necessary while correctness is determined by argmax though due to monotonicity of softmax
+    answer1 = tf.contrib.layers.fully_connected(prevNetworkOutput, dictSize, activation_fn=tf.nn.relu)#tf.nn.softmax)#shape=(batch_size, dictSize)
+    answer2 = tf.contrib.layers.fully_connected(prevNetworkOutput, dictSize, activation_fn=tf.nn.relu)#tf.nn.softmax)
+    answer3 = tf.contrib.layers.fully_connected(prevNetworkOutput, dictSize, activation_fn=tf.nn.relu)#tf.nn.softmax)
+    answerGates = tf.contrib.layers.fully_connected(prevNetworkOutput, 3, activation_fn=tf.sigmoid)#shape=(batch_size, 3)
+    answerStack = tf.stack([answer1, answer2, answer3], axis=1)#stack shape=(batch_size, 3, dictSize)
+    answer = tf.reduce_sum(tf.multiply(answerStack, tf.expand_dims(answerGates, axis=2)), axis=1)
+
+    return answer, answerGates
+
+def buildOptimizer(answer, answerGates):
+    inputAnswer = tf.placeholder(tf.float32, shape=(batch_size, dictSize))#label
+    #loss = tf.losses.mean_squared_error(labels=inputAnswer, predictions=answer) * dictSize - tf.reduce_mean(tf.square(answerGates - 0.5)) + 0.25#regularization term to enforce gate values close to 0 or 1
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=answer, labels=inputAnswer)) - tf.reduce_mean(tf.square(answerGates - 0.5)) + 0.25#regularization term to enforce gate values close to 0 or 1
+    #softmax_cross_entropy_with_logits is not suitable for outputs that are not probability distributions (which might be a problem for multi-answer questions) - still gives surprisingly good results for a first attempt
+    optimizer_op = tf.train.AdamOptimizer(1e-5).minimize(loss)
+
+    return inputAnswer, loss, optimizer_op
+
 #utility functions
 def getIndices(dataset, epochs):#generate indices over all epochs
     for epoch in range(epochs):
@@ -465,23 +498,57 @@ def getBatches(dataset, epochs):#generate batches of data
         answerInput = [answer for context, question, answer in samples]
         yield contextInput, contextLengths, contextSentenceLengths, questionInput, questionLengths, answerInput
 
-#load dictionary
-with open(os.path.join('processeddata', 'dictionary.txt'), 'rb') as f:
-    wordIndices = pickle.load(f)
-dictSize = len(wordIndices) + 1#padding entry with index 0 is not listed in wordIndices
+(inputContext, inputContextLengths, inputContextSentenceLengths, inputQuestion, inputQuestionLengths, objects, question) = buildWordProcessorLSTMs()
 
-#load data
-with open(os.path.join('processeddata', 'train.txt'), 'rb') as f:
-    trainingData = pickle.load(f)
+rnOutput = buildRN_I(objects, question)
 
-with open(os.path.join('processeddata', 'valid.txt'), 'rb') as f:
-    validationData = pickle.load(f)
+(answer, answerGates) = buildAnswerModel(rnOutput)
+
+(inputAnswer, loss, optimizer_op) = buildOptimizer(answer, answerGates)
+
+def runValidation():
+    #correct = tf.reduce_min(tf.cast(tf.equal(inputAnswer, tf.round(answer)), dtype=tf.float32), axis=1)#bad results since the max entries often don't achieve 0.5 so rounding doesnt work
+    correct = tf.cast(tf.equal(tf.argmax(inputAnswer, axis=1), tf.argmax(answer, axis=1)), dtype=tf.float32)#this is incorrect for multi-answer questions but gives better answers than rounding on single-answer questions -> TODO: find good solution for multi-answer questions
+    #idea for better implementation of "correct"-variable: take argmax of answer1, answer2, answer3 each, also round answerGates and then calculate "answer" similar as in "buildModel()" and finally check tf.equal
+    accuracy = tf.reduce_mean(correct)
+    total_acc = []
+    for task_name in validationData:
+        print("validating " + task_name)
+        acc = []
+        for i, (contextInput, contextLengths, contextSentenceLengths, questionInput, questionLengths, answerInput) in enumerate(getBatches(validationData[task_name], 1)):
+            #print("validation batch " + str(i))
+            feed_dict={inputContext: contextInput, inputContextLengths: contextLengths, inputContextSentenceLengths: contextSentenceLengths, inputQuestion: questionInput, inputQuestionLengths: questionLengths, inputAnswer: answerInput}
+            batchAcc = sess.run(accuracy, feed_dict)
+            acc.append(batchAcc)
+            # print("label")
+            # print(answerInput)
+            # print("prediction")
+            # print(sess.run(answer, feed_dict))
+            # print("correct")
+            # print(sess.run(correct, feed_dict))
+            # print("gates")
+            # print(sess.run(answerGates, feed_dict))
+        taskAcc = sum(acc) / len(acc)
+        print("task accuracy " + str(taskAcc))
+        total_acc.append(taskAcc)
+    print("total accuracy " + str(sum(total_acc) / len(total_acc)))
+
+def train():
+    for i, (contextInput, contextLengths, contextSentenceLengths, questionInput, questionLengths, answerInput) in enumerate(getBatches(trainingData, epoch_count)):
+        feed_dict={inputContext: contextInput, inputContextLengths: contextLengths, inputContextSentenceLengths: contextSentenceLengths, inputQuestion: questionInput, inputQuestionLengths: questionLengths, inputAnswer: answerInput}
+        sess.run(optimizer_op, feed_dict=feed_dict)
+        lossVal = sess.run(loss, feed_dict=feed_dict)
+        if (i % 50 == 0):
+            print("batch " + str(i))
+            print("loss " + str(lossVal))
+        if (i % 1000 == 999):
+            runValidation()
 
 #print(sess.run(resTensorB))
-print(sess.run(getHeteroCombinations(testTensorC, testTensorC)))
-print(sess.run(getCombinations(testTensorC)))
-print(sess.run(getTransitiveCombine(getCombinations(testTensorC))))
+# print(sess.run(getHeteroCombinations(testTensorC, testTensorC)))
+# print(sess.run(getCombinations(testTensorC)))
+# print(sess.run(getTransitiveCombine(getCombinations(testTensorC))))
 
-print(sess.run(getTripleCombinations(testTensorC)))
+# print(sess.run(getTripleCombinations(testTensorC)))
 
 #writer = tf.summary.FileWriter('log', sess.graph)
